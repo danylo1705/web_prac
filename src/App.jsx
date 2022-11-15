0bee9e82145ce286d9bc780387aebb45f65efe0a
danylo1705 <49870659+danylo1705@users.noreply.github.com>
2022-11-15 11:24:15 +0100

    clean_repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-import Content from './components/content/Content';
-import Sidebar from './components/sidebar/sidebar';
+
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='app'>
-      <Sidebar/>
-        <Routes>
-          <Route path='/' element={<Content/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/contact' element={<Contact/>} />
-        </Routes>
-        
-        
-      </div>
-    </BrowserRouter>
+   <div className="App"></div>
   );
 }
 
